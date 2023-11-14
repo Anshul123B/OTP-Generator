ca081fd6008904bfce05145a5c3d3b6c912812cb
Anshul Bhatt <134294749+Anshul123B@users.noreply.github.com>
2023-11-14 20:24:51 +0530

    Add files via upload

---
+++ OTP-Generator/script.js
@@ -0,0 +1,15 @@
+function otpGenerate() {
+    let otpShow = document.getElementById("otp");
+    let digits = "0123456789";
+    let otp = "";
+
+    for (let i = 0; i < 4; i++) {
+        otp += digits[Math.floor(Math.random() * 10)];
+    }
+
+    otpShow.innerHTML = "<span></span>";
+
+    setTimeout(() => {
+        otpShow.innerHTML = otp;
+    }, 1000);
+}
